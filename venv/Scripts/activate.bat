4ece8e8fe9fdbace979b9777144c09c30f656888
Johana Guardado <johana.guardado@mtecorp.com>
2026-03-05 07:59:22 -0800

    admin available

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\guardaj\Documents\mte_johana_guardado_azure\oee-mte\venv"
+set "VIRTUAL_ENV=C:\Users\guardaj\Documents\mte_johana27\venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
