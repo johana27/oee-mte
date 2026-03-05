26f1ad28a49307311e7a2486f5f8a77f05e13fdf
Johana Guardado <johana.guardado@mtecorp.com>
2026-03-05 09:58:41 -0800

    trigger azure deploy

--- system/system/settings.py
+++ system/system/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+#Comentario para disparar el 1er deploy
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
