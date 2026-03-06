e9fbe70bc7494b1c72beb4f23b62d110076175b2
johana27 <johanaguardado.274@gmail.com>
2026-03-06 12:08:46 -0800

    modificar el path de accounts en settings

--- system/system/settings.py
+++ system/system/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts',
+    'system.accounts',
     'core',
     'manufacturing',
     'planning',
