d2e8e1ec5afdabaf38583f877f3f52a7f9a2c58e
johana27 <johanaguardado.274@gmail.com>
2026-03-10 11:02:04 -0700

    accounts path

--- system/accounts/apps.py
+++ system/accounts/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
-
+#path accounts
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'accounts'
+    name = "system.accounts"
