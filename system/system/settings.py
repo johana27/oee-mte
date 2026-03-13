4990dc5ad02a110b30b5c055c288bf2c15b7a9e3
johana27 <johanaguardado.274@gmail.com>
2026-03-13 10:46:25 -0700

    sqlite same database

--- system/system/settings.py
+++ system/system/settings.py
@@ -120,21 +120,6 @@ DATABASES = {
 }
 
 
-if os.getenv("DATABASE_URL"):
-    DATABASES = {
-    "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL")
-    )
-}
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
